0c868251221a459ddca1cde07e558d296f974c55
Andrew Pacheco <ndrewpacheco@gmail.com>
2018-09-07 15:18:04 -0500

    finished loops

---
+++ Loops/ex2.rb
@@ -0,0 +1,9 @@
+# ex2.rb
+
+input = ""
+while input != "STOP" do
+  puts "what is next?"
+  input = gets.chomp
+end
+
+
---
+++ Loops/ex3.rb
@@ -0,0 +1,4 @@
+
+arr = ["drew", 2, "sup"]
+
+arr.each_with_index {|name, index| puts "#{index + 1}: #{name}"}
